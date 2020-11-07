241634f897c2c8a4c7d4b541e57fc5c261ec793f
Sohag <sm.sohag1@gmail.com>
2020-11-08 03:44:25 +0600

    Creating Menu Component

---
+++ src/components/body/Menu.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import DISHES from '../../data/dishes.js';
+import MenuItem from './MenuItem';
+
+class Menu extends Component {
+    state = {
+        dishes: DISHES
+    }
+
+    render() {
+        const menu = this.state.dishes.map(item => {
+            return (
+                <MenuItem dish={item} key={item.id} />
+            );
+        })
+        return (
+            <div className="container">
+                <div clasName="row">
+                    <div className="col-6">
+                        {menu}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Menu;
