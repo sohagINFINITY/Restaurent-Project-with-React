5af8bdd7c4f214bb1dc37fcd403e85d2e34a7847
Sohag <sm.sohag1@gmail.com>
2020-11-29 13:17:59 +0600

    comment form

--- src/components/body/Menu.js
+++ src/components/body/Menu.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import DISHES from '../../data/dishes.js';
-import COMMENTS from '../../data/comments.js';
 import MenuItem from './MenuItem';
 import DishDetail from './DishDetail';
 import { CardColumns, Modal, ModalBody, ModalFooter, Button } from 'reactstrap';
+import { connect } from 'react-redux';
 
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments
+    }
+}
 
 class Menu extends Component {
     state = {
-        dishes: DISHES,
-        comments: COMMENTS,
         selectedDish: null,
         modalOpen: false
     }
@@ -29,7 +32,7 @@ class Menu extends Component {
 
     render() {
         document.title = "Menu";
-        const menu = this.state.dishes.map(item => {
+        const menu = this.props.dishes.map(item => {
             return (
                 <MenuItem
                     dish={item}
@@ -41,7 +44,7 @@ class Menu extends Component {
 
         let dishDetail = null;
         if (this.state.selectedDish != null) {
-            const comments = this.state.comments.filter(comment => comment.dishId === this.state.selectedDish.id
+            const comments = this.props.comments.filter(comment => comment.dishId === this.state.selectedDish.id
             )
             dishDetail = <DishDetail
                 dish={this.state.selectedDish}
@@ -53,7 +56,7 @@ class Menu extends Component {
                     <CardColumns>
                         {menu}
                     </CardColumns>
-                    <Modal isOpen={this.state.modalOpen} onClick={this.toggleModal}>
+                    <Modal isOpen={this.state.modalOpen}>
                         <ModalBody>
                             {dishDetail}
                         </ModalBody>
@@ -69,4 +72,4 @@ class Menu extends Component {
     }
 }
 
-export default Menu;
+export default connect(mapStateToProps)(Menu);
